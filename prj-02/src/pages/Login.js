b76de0b9ad17a54c2716191f39be3d287423c167
seethalammurali <muralikrishnaseethalam@gmail.com>
2025-06-17 23:58:51 +0530

    Login Page and CSS

--- prj-02/src/pages/Login.js
+++ prj-02/src/pages/Login.js
@@ -4,9 +4,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { setLogin } from "../redux/authSlice";
 import { useLoginMutation } from "../slices/usersApiSlice";
-import { toast } from "react-toastify";
-import logo from "../assets/logo/TheQucikPayMe.png"
-import sideImage from "../assets/logo/left-side.jpeg"
+import logo from "../assets/logo/TheQucikPayMe.png";
+import leftside1 from "../assets/logo/QuickPayLoginPage1.png";
+import leftside2 from "../assets/logo/QuickPayLoginPage2.png";
+import rightside1 from "../assets/logo/QuickPayLoginPage1.png";
+import rightside2 from "../assets/logo/QuickPayLoginPage2.png";
+import rightside3 from "../assets/logo/QuickPayLoginPage1.png";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function Login() {
@@ -17,7 +20,7 @@ function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { userInfo } = useSelector((state) => state.auth);
-  const [login, { isLoading }] = useLoginMutation();
+  const [login] = useLoginMutation();
 
   useEffect(() => {
     if (userInfo) {
@@ -31,30 +34,38 @@ function Login() {
 
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async (position) => {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
+        const { latitude, longitude } = position.coords;
         try {
-          const res = await login({ userid: userID, password, geoLocation: { latitude, longitude } }).unwrap();
+          const res = await login({
+            userid: userID,
+            password,
+            geoLocation: { latitude, longitude },
+          }).unwrap();
           dispatch(setLogin({ ...res }));
           navigate("/dashboard");
         } catch (err) {
           setError("Please Enter Valid Credentials");
         }
-      }, (error) => {
-        console.error('Geolocation error:', error.message);
-      })
+      });
     } else {
-      console.log('Geo location is not supported by this browser');
+      console.log("Geolocation not supported");
     }
   };
 
   return (
     <div className="vh-100 d-flex">
-      <div className="col-md-7 left-section d-flex justify-content-center align-items-center">
-        <img src={sideImage} alt="Login Visual" className="img-fluid" />
-      </div>
-      <div className="col-md-5 right-section d-flex flex-column justify-content-center align-items-center position-relative">
+   <div className="col-md-8 left-section d-flex justify-content-center align-items-center position-relative">
+   <img src={leftside1} alt="Left Image 1" className="corner-image top-left-1" />
+<img src={leftside2} alt="Left Image 2" className="corner-image top-left-2" />
+<img src={rightside1} alt="Right Image 1" className="corner-image top-right-1" />
+<img src={rightside2} alt="Right Image 2" className="corner-image top-right-2" />
+<img src={rightside3} alt="Right Image 3" className="corner-image top-right-3" />
+  
+</div>
+
+
+
+      <div className="col-md-4 right-section d-flex flex-column justify-content-center align-items-center position-relative">
         <div className="login-box w-100">
           <div className="d-flex flex-column align-items-center">
             <img src={logo} alt="Blender Logo" className="logo-img" />
@@ -85,28 +96,23 @@ function Login() {
               </div>
               <div className="d-flex justify-content-between mb-4">
                 <span>
-                  <a href="#" className="text-primary">
-                    Forgot Password?
-                  </a>
+                  <a href="#" className="text-primary">Forgot Password?</a>
                 </span>
                 <button type="submit" className="btn btn-primary">Login</button>
               </div>
             </form>
 
-            <p className="mt-4 text-center text-muted">&copy; All rights received @2025 NeoFin Nex India Pvt Ltd</p>
+            <p className="mt-4 text-center text-muted">
+              &copy; All rights reserved @2025 NeoFin Nex India Pvt Ltd
+            </p>
           </div>
         </div>
-        {/* Footer links moved to bottom */}
+
         <div className="footer-links text-center">
           <small>
-          For more information{" "}
-            <Link to="/terms" className="text-primary text-decoration-underline">
-              T&C
-            </Link>{" "}
-            and{" "}
-            <Link to="/privacypolicy" className="text-primary text-decoration-underline">
-              Privacy Policies
-            </Link>.
+            For more information{" "}
+            <Link to="/terms" className="text-primary text-decoration-underline">T&C</Link> and{" "}
+            <Link to="/privacypolicy" className="text-primary text-decoration-underline">Privacy Policies</Link>.
           </small>
         </div>
       </div>
